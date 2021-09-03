ad036f110d9f794bb85e4c890fd221137c092c8c
EwarystDebian <ewaryst@spoko.pl>
2021-09-03 19:34:56 +0200

    test

--- src/App.js
+++ src/App.js
@@ -26,7 +26,7 @@ function App() {
           ]}
           RightElement={(
             <div>
-              <Button variant="inline" onClick={() => i18n.changeLanguage('pl')}>ple</Button>
+              <Button variant="inline" onClick={() => i18n.changeLanguage('pl')}>pl</Button>
               <Button variant="inline" onClick={() => i18n.changeLanguage('en')}>en</Button>
             </div>
           )} />
